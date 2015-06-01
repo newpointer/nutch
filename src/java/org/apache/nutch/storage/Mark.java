4bdc0bab39ede7c01ee057c3429f5f0e90d5e48f
Maksim Konyuhov <konyuhov.maksim@gmail.com>
2015-06-01 18:34:13 +0300

    fixed checkMark

--- src/java/org/apache/nutch/storage/Mark.java
+++ src/java/org/apache/nutch/storage/Mark.java
@@ -5,9 +5,9 @@
  * The ASF licenses this file to You under the Apache License, Version 2.0
  * (the "License"); you may not use this file except in compliance with
  * the License.  You may obtain a copy of the License at
- * 
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -41,12 +41,18 @@ public enum Mark {
   }
 
   public Utf8 checkMark(WebPage page) {
-    return (Utf8) page.getMarkers().get(name);
+    Utf8 utf8 = (Utf8) page.getMarkers().get(name);
+
+    if (utf8 == null || utf8.length() == 0) {
+      return null;
+    }
+
+    return utf8;
   }
 
   /**
    * Remove the mark only if the mark is present on the page.
-   * 
+   *
    * @param page
    *          The page to remove the mark from.
    * @return If the mark was present.
